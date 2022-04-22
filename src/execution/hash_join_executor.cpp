2a5253b5a4523e666023cdad09dd35026b0f7b1d
hewei <191250044@smail.nju.edu.cn>
2022-04-22 17:35:45 +0800

    Hash join executor

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -17,10 +17,85 @@ namespace bustub {
 HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&left_child,
                                    std::unique_ptr<AbstractExecutor> &&right_child)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), left_executor_(std::move(left_child)),
+        right_executor_(std::move(right_child)), left_predictor_(nullptr), right_preidctor_(nullptr), cur_pos_(0) {
+        left_predictor_ = plan_->LeftJoinKeyExpression();
+        right_preidctor_ = plan_->RightJoinKeyExpression();
+        if (left_predictor_ == nullptr) {
+            left_predictor_ = new ConstantValueExpression(ValueFactory::GetBooleanValue(true));
+        }
+        if (right_preidctor_ == nullptr) {
+            right_preidctor_ = new ConstantValueExpression(ValueFactory::GetBooleanValue(true));
+        }
 
-void HashJoinExecutor::Init() {}
+        left_executor_->Init();
+        Tuple left_tuple;
+        RID left_rid;
+        while (left_executor_->Next(&left_tuple, &left_rid)) {
+            const Schema *left_output_schema = plan_->GetLeftPlan()->OutputSchema();
 
-bool HashJoinExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+            std::vector<Value> values;
+            values.reserve(left_output_schema->GetColumnCount());
+            for (const auto &col : left_output_schema->GetColumns()) {
+                values.emplace_back(col.GetExpr()->Evaluate(&left_tuple, left_output_schema));
+            }
+            Value val = left_predictor_->Evaluate(&left_tuple, left_output_schema);
+            HashJoinKey key{val};
+            hash_map_[key].emplace_back(std::move(values));
+        }
+    }
+
+HashJoinExecutor::~HashJoinExecutor() {
+    if (left_predictor_ != plan_->LeftJoinKeyExpression()) {
+        delete left_predictor_;
+    }
+    if (right_preidctor_ != plan_->RightJoinKeyExpression()) {
+        delete right_preidctor_;
+    }
+}
+
+void HashJoinExecutor::Init() {
+    left_executor_->Init();
+    right_executor_->Init();
+}
+
+bool HashJoinExecutor::Next(Tuple *tuple, RID *rid) {
+    const Schema *right_output_schema = plan_->GetRightPlan()->OutputSchema();
+    Value val;
+    if (tuple->GetData() != nullptr)
+        val = right_preidctor_->Evaluate(tuple, right_output_schema);
+    if (val.GetTypeId() == TypeId::INVALID || cur_pos_ >= hash_map_[HashJoinKey{val}].size()) {
+        if (val.GetTypeId() != TypeId::INVALID && cur_pos_ == 0) {
+            hash_map_.erase(HashJoinKey{val});
+        }
+        while (right_executor_->Next(tuple, rid)) {
+            val = right_preidctor_->Evaluate(tuple, right_output_schema);
+            if (hash_map_.find(HashJoinKey{val}) != hash_map_.end()) {
+                cur_pos_ = 0;
+                break;
+            }
+        }
+        if (cur_pos_ != 0) {
+            return false;
+        }
+    }
+    if (hash_map_.find(HashJoinKey{val}) != hash_map_.end()) {
+        std::vector<Value> values;
+        values.reserve(plan_->OutputSchema()->GetColumnCount());
+
+        for (const auto &col : plan_->OutputSchema()->GetColumns()) {
+            auto expr = reinterpret_cast<const ColumnValueExpression *>(col.GetExpr());
+            if (expr->GetTupleIdx() == 0) {
+                values.push_back(hash_map_[HashJoinKey{val}][cur_pos_][expr->GetColIdx()]);
+            } else {
+                values.push_back(tuple->GetValue(right_output_schema, expr->GetColIdx()));
+            }                
+        }
+        *tuple = Tuple(values, plan_->OutputSchema());
+        cur_pos_++;
+        return true;
+    }
+    return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/hash_join_executor.h
+++ src/include/execution/executors/hash_join_executor.h
@@ -14,11 +14,34 @@
 
 #include <memory>
 #include <utility>
+#include <vector>
+#include <unordered_map>
 
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/hash_join_plan.h"
 #include "storage/table/tuple.h"
+#include "common/util/hash_util.h"
+#include "execution/expressions/column_value_expression.h"
+#include "execution/expressions/constant_value_expression.h"
+#include "type/value_factory.h"
+
+namespace bustub {
+
+struct HashJoinKey {
+  Value key_;
+
+  bool operator==(const HashJoinKey &other) const { return key_.CompareEquals(other.key_) == CmpBool::CmpTrue; }
+};
+}  // namespace bustub
+
+namespace std {
+
+template <>
+struct hash<bustub::HashJoinKey> {
+  std::size_t operator()(const bustub::HashJoinKey &key) const { return bustub::HashUtil::HashValue(&key.key_); }
+};
+}  // namespace std
 
 namespace bustub {
 
@@ -37,6 +60,9 @@ class HashJoinExecutor : public AbstractExecutor {
   HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlanNode *plan,
                    std::unique_ptr<AbstractExecutor> &&left_child, std::unique_ptr<AbstractExecutor> &&right_child);
 
+  /** Deconstruct the hash join executor instance */
+  ~HashJoinExecutor() override;
+
   /** Initialize the join */
   void Init() override;
 
@@ -54,6 +80,18 @@ class HashJoinExecutor : public AbstractExecutor {
  private:
   /** The NestedLoopJoin plan node to be executed. */
   const HashJoinPlanNode *plan_;
+  /** Left child executor */
+  std::unique_ptr<AbstractExecutor> left_executor_;
+  /** Right child executor */
+  std::unique_ptr<AbstractExecutor> right_executor_;
+  /** Hashmap for the outer(left) table */
+  std::unordered_map<HashJoinKey, std::vector<std::vector<Value>>> hash_map_;
+  /** Left predictor*/
+  mutable const AbstractExpression *left_predictor_;
+  /** Right predictor */
+  mutable const AbstractExpression *right_preidctor_;
+  /** CurPos of the out(left) table tuple */
+  uint32_t cur_pos_;
 };
 
 }  // namespace bustub
