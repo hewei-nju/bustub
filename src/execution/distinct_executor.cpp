9cbe6ece7aa6fe3435974c3275057425177d290e
hewei <191250044@smail.nju.edu.cn>
2022-04-23 00:07:46 +0800

    Distinct executor

--- src/execution/distinct_executor.cpp
+++ src/execution/distinct_executor.cpp
@@ -16,10 +16,25 @@ namespace bustub {
 
 DistinctExecutor::DistinctExecutor(ExecutorContext *exec_ctx, const DistinctPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void DistinctExecutor::Init() {}
+void DistinctExecutor::Init() {
+    child_executor_->Init();
+}
 
-bool DistinctExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool DistinctExecutor::Next(Tuple *tuple, RID *rid) {
+    while (child_executor_->Next(tuple, rid)) {
+        DistinctKey key;
+        key.group_bys_.reserve(plan_->OutputSchema()->GetColumnCount());
+        for (uint32_t i = 0; i < plan_->OutputSchema()->GetColumnCount(); i++) {
+            key.group_bys_.emplace_back(tuple->GetValue(plan_->OutputSchema(), i));
+        }
+        if (hash_set_.find(key) == hash_set_.end()) {
+            hash_set_.emplace(std::move(key));
+            return true;
+        }
+    }
+    return false;
+}
 
 }  // namespace bustub
--- src/include/execution/executors/distinct_executor.h
+++ src/include/execution/executors/distinct_executor.h
@@ -17,6 +17,40 @@
 
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/distinct_plan.h"
+#include "common/util/hash_util.h"
+#include "execution/expressions/column_value_expression.h"
+
+namespace bustub {
+
+struct DistinctKey {
+  std::vector<Value> group_bys_;
+
+  bool operator==(const DistinctKey &other) const {
+    for (std::size_t i = 0; i < other.group_bys_.size(); i++) {
+      if (group_bys_[i].CompareEquals(other.group_bys_[i]) != CmpBool::CmpTrue) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+}  // namespace bustub
+
+namespace std {
+
+template <>
+struct hash<bustub::DistinctKey> {
+  std::size_t operator()(const bustub::DistinctKey &distinct_key) const {
+    size_t hash_val = 0;
+    for (const auto &key : distinct_key.group_bys_) {
+      if (!key.IsNull()) {
+        hash_val = bustub::HashUtil::CombineHashes(hash_val, bustub::HashUtil::HashValue(&key));
+      }
+    }
+    return hash_val;
+  }
+};
+}  // namespace std
 
 namespace bustub {
 
@@ -53,5 +87,7 @@ class DistinctExecutor : public AbstractExecutor {
   const DistinctPlanNode *plan_;
   /** The child executor from which tuples are obtained */
   std::unique_ptr<AbstractExecutor> child_executor_;
+   /** Hashset for the preview tuple */
+  std::unordered_set<DistinctKey> hash_set_;
 };
 }  // namespace bustub
